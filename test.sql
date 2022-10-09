3f35b747af7db9dd08fb1725ccaf96ddf47a48d9
Valeriy <valeriy@manenkov.com>
2022-10-09 22:12:30 +0300

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,8 @@
+call insert_task(title := 'Make a init-sql in Spring Boot-1.4.1-RELEASE');
+call insert_task(title := 'Configure port for a Spring Boot application');
+call insert_task(title := 'Running code after Spring Boot starts');
+call insert_task(title := 'Write result of a differentiation in terms of dependent variable');
+call insert_task(title := 'Use of the subjunctive in a quod-clause in Renaissance Latin');
+call insert_task(title := 'Finding duplicate blocks of text within a file using shell script');
+
+select * from todo_list();
