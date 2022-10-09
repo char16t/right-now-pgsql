0b623a2e9a6074b5e6124542b2c954a791e629a4
Valeriy <valeriy@manenkov.com>
2022-10-09 22:13:11 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -45,14 +45,4 @@ begin
 end;
 $$ language plpgsql;
 
-
 insert into tasks(parent_id, task_order, title) values (null, 1, 'Meta task');
-
-call insert_task(title := 'Make a init-sql in Spring Boot-1.4.1-RELEASE');
-call insert_task(title := 'Configure port for a Spring Boot application');
-call insert_task(title := 'Running code after Spring Boot starts');
-call insert_task(title := 'Write result of a differentiation in terms of dependent variable');
-call insert_task(title := 'Use of the subjunctive in a quod-clause in Renaissance Latin');
-call insert_task(title := 'Finding duplicate blocks of text within a file using shell script');
-
-select * from todo_list();
