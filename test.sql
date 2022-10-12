8299480945245ee94c45727a0d783a845f257f6e
Valeriy <valeriy@manenkov.com>
2022-10-12 21:28:07 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,5 @@
+insert into tasks(parent_id, task_order, title) values (null, 1, 'Meta task');
+
 call insert_task(parent := 1, title := 'AA');
 call insert_task(parent := 2, title := 'AAA');
 call insert_task(parent := 2, title := 'AAB');
@@ -10,9 +12,10 @@ call insert_task(parent := 1, title := 'AD');
 call insert_task(parent := 1, title := 'AE');
 call insert_task(parent := 1, title := 'AF');
 
-call task_done(task_id := 2);
+
+update tasks t set "task_status" = 'DONE'::"task_status" where t.id = 2;
+update tasks t set "task_status" = 'DONE'::"task_status" where t.id = 9;
+--update tasks t set "task_status" = 'TODO'::"task_status" where t.id = 2;
 call delete_task(task_id := 5);
-
-select * from todo_list();
-
 select * from tasks;
+select * from todo_list();
