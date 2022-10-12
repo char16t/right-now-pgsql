0e88a9aa4ec7744514906cbd6124e6c3c03ad1ad
Valeriy <valeriy@manenkov.com>
2022-10-12 12:06:02 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,9 +1,9 @@
-call insert_task(title := 'Make a init-sql in Spring Boot-1.4.1-RELEASE');
-call insert_task(title := 'Configure port for a Spring Boot application');
-call insert_task(title := 'Running code after Spring Boot starts');
-call insert_task(title := 'Write result of a differentiation in terms of dependent variable');
-call insert_task(title := 'Use of the subjunctive in a quod-clause in Renaissance Latin');
-call insert_task(title := 'Finding duplicate blocks of text within a file using shell script');
+call insert_task(parent := 1, title := 'AA');
+call insert_task(parent := 1, title := 'AB');
+call insert_task(parent := 1, title := 'AC');
+call insert_task(parent := 1, title := 'AD');
+call insert_task(parent := 1, title := 'AE');
+call insert_task(parent := 1, title := 'AF');
 
 call reorder_task(task_id := 5, new_order := 2);
 call task_done(task_id := 2);
