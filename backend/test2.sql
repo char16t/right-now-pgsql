7a7bd123c0bf3b00f4462950488a44175707b80c
Valeriy <valeriy@manenkov.com>
2022-10-27 20:58:58 +0300

    Update test2.sql

--- backend/test2.sql
+++ backend/test2.sql
@@ -1,31 +1,42 @@
-insert into tasks(id, parent_id, title, expanded) 
-	values (1, null, 'A', true);
+insert into tasks(id, parent_id, expanded) values (1, null, true);
+insert into task_info(id, title) values (1, 'A');
 
-insert into tasks(id, parent_id, title, expanded, "task_type") 
-	values (2, 1, 'AA', true, 'ORDERED');
-insert into tasks(id, parent_id, title, expanded) 
-	values (3, 2, 'AAA', true);
+insert into tasks(id, parent_id, expanded, "task_type") values (2, 1, true, 'ORDERED');
+insert into task_info(id, title) values (2, 'AA');
+
+insert into tasks(id, parent_id, expanded) values (3, 2, true);
+insert into task_info(id, title) values (3, 'AAA');
+
 update tasks set "task_status" = 'DONE' where id = 3;
-insert into tasks(id, parent_id, title, expanded) 
-	values (4, 2, 'AAB', true);
-insert into tasks(id, parent_id, title, expanded) 
-	values (5, 2, 'AAC', true);
 
-insert into tasks(id, parent_id, title, expanded) 
-	values (6, 1, 'AB', false);
-insert into tasks(id, parent_id, title, expanded) 
-	values (7, 6, 'ABA', false);
-insert into tasks(id, parent_id, title, expanded) 
-	values (8, 6, 'ABB', false);
-insert into tasks(id, parent_id, title, expanded) 
-	values (9, 6, 'ABC', false);
+insert into tasks(id, parent_id, expanded) values (4, 2, true);
+insert into task_info(id, title) values (4, 'AAB');
 
+insert into tasks(id, parent_id, expanded) values (5, 2, true);
+insert into task_info(id, title) values (5, 'AAC');
 
-insert into tasks(id, parent_id, title, expanded, "task_type") 
-	values (10, 1, 'AC', true, 'ONE_OF');
-insert into tasks(id, parent_id, title, expanded) 
-	values (11, 10, 'ACA', false);
-insert into tasks(id, parent_id, title, expanded) 
-	values (12, 10, 'ACB', false);
-insert into tasks(id, parent_id, title, expanded) 
-	values (13, 10, 'ACC', false);
+insert into tasks(id, parent_id, expanded) values (6, 1, false);
+insert into task_info(id, title) values (6, 'AB');
+
+insert into tasks(id, parent_id, expanded) values (7, 6, false);
+insert into task_info(id, title) values (7, 'ABA');
+
+insert into tasks(id, parent_id, expanded) values (8, 6, false);
+insert into task_info(id, title) values (8, 'ABB');
+
+insert into tasks(id, parent_id, expanded) values (9, 6, false);
+insert into task_info(id, title) values (9, 'ABC');
+
+insert into tasks(id, parent_id, expanded, "task_type") values (10, 1, true, 'ONE_OF');
+insert into task_info(id, title) values (10, 'AC');
+
+insert into tasks(id, parent_id, expanded) values (11, 10, false);
+insert into task_info(id, title) values (11, 'ACA');
+
+insert into tasks(id, parent_id, expanded) values (12, 10, false);
+insert into task_info(id, title) values (12, 'ACB');
+
+insert into tasks(id, parent_id, expanded) values (13, 10, false);
+insert into task_info(id, title) values (13, 'ACC');
+
+select * from todo_list(1);
